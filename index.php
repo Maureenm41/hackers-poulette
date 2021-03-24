cf3278140da2a530c1d7afb17ac85083712d53b0
Maureenm41 <maureenmrchl@gmail.com>
2021-03-24 11:00:05 +0100

    test php

--- index.php
+++ index.php
@@ -65,6 +65,28 @@
         echo "error , you need to send the form!";
     };*/
 
+    $errors = [];
+
+if (!empty($_POST)) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+  
+   if (empty($name)) {
+       $errors[] = 'Name is empty';
+   }
+
+   if (empty($email)) {
+       $errors[] = 'Email is empty';
+   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $errors[] = 'Email is invalid';
+   }
+
+   if (empty($message)) {
+       $errors[] = 'Message is empty';
+   }
+}
+
 ?>
 
 <?php include 'form.php';?>
