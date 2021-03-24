7a5b4592d7b67879bef1ca89aecb0396a0a7b36f
Maureenm41 <maureenmrchl@gmail.com>
2021-03-24 15:19:08 +0100

    test

--- index.php
+++ index.php
@@ -9,11 +9,12 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="styles.css.map">
     <link rel="stylesheet" href="styles.scss">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Bellota:ital,wght@1,300&display=swap');
     </style>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Bellota:ital,wght@1,300&display=swap" rel="stylesheet">
     <title>Hackers Poulette Form</title>
 </head>
 <body>
