b874cdb36719ceda5f56db0d93163be7757c3826
Maureenm41 <maureenmrchl@gmail.com>
2021-03-24 14:30:36 +0100

    up

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description">
+    <meta name="description" content="contact form">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="assets/hackers-poulette-logo.png" />
     <link rel="stylesheet" href="styles.css">
